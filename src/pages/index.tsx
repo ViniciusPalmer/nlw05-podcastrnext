21b2db860875418dceaf4d4e93acdcbd3426716a
unknown <viniciuspncandido@gmail.com>
2021-05-14 00:46:44 -0300

    Adicionando static props dinamic no SSG da page index.tsx e configurando o framework axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,51 @@
-export default function Home(props) {
-  console.log(props.episodes);
+import { GetStaticProps } from "next";
+import { format, parseISO } from "date-fns";
+import ptBr from "date-fns/locale/pt-BR";
+import { api } from "../services/api";
+import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
+
+import styles from "./home.module.scss";
+
+type Episode = {
+  id: string;
+  title: string;
+  thumbnail: string;
+  description: string;
+  members: string;
+  duration: number;
+  durationAsString: string;
+  url: string;
+  publishedAt: string;
+};
+
+type HomeProps = {
+  latesEpisodes: Episode[];
+  allEpisodes: Episode[];
+};
+
+export default function Home({ latesEpisodes, allEpisodes }: HomeProps) {
   return (
-    <div>
-      <h1>Index</h1>
+    <div className={styles.homePage}>
+      <section className={styles.latestEpisodes}>
+        <h2>Últimos Lançamentos</h2>
+        <ul>
+          {latesEpisodes.map((episode) => {
+            return (
+              <li>
+                <a href="">{episode.title}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+
+      <section className={styles.allEpisodes}></section>
     </div>
   );
 }
 
 // Função nativa do NEXT que permite o uso de:
-// SSR = SERVER SITE RENDERING - executa sempre que a pagina é carregado
+// SSR = SERVER SITE RENDERING - executa sempre que a pagina é carregada
 /*
 export async function getServerSideProps() {
   const response = await fetch("http://localhost:3333/episodes");
@@ -24,14 +61,42 @@ export async function getServerSideProps() {
 
 // SSG = SERVER SITE GENERETION - executa quando setado | só funciona em prod
 
-export async function getStaticProps() {
-  const response = await fetch("http://localhost:3333/episodes");
-  const data = await response.json();
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get("episodes", {
+    params: {
+      _limit: 12,
+      _sort: "published",
+      _order: "desc",
+    },
+  });
+
+  console.log(data);
+
+  const episodes = data.map((episode) => {
+    return {
+      id: episode.id,
+      title: episode.id,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), "d MMM yy", {
+        locale: ptBr,
+      }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(
+        Number(episode.file.duration)
+      ),
+      url: episode.file.url,
+    };
+  });
+
+  const latesEpisodes = episodes.slice(0, 2);
+  const allEpisodes = episodes.slice(2, episodes.length);
 
   return {
     props: {
-      episodes: data,
+      latesEpisodes,
+      allEpisodes,
     },
     revalidate: 60 * 60 * 8,
   };
-}
+};
